55a372a4c9c04f9aa14076d1ccd3a38d8bf79146
Damiano Mazzella <damianomazzella@gmail.com>
2022-06-11 15:51:42 +0200

    Removed last param of MP_REGISTER_MODULE

--- moducrypto.c
+++ moducrypto.c
@@ -1845,4 +1845,4 @@ const mp_obj_module_t mp_module_ucrypto = {
 };
 
 // Register the module to make it available in Python
-MP_REGISTER_MODULE(MP_QSTR__crypto, mp_module_ucrypto, MICROPY_PY_UCRYPTO);
+MP_REGISTER_MODULE(MP_QSTR__crypto, mp_module_ucrypto);
