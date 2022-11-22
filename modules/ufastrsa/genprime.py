f3993d950e4565620551bbbfa51488ce23fb584c
Damiano Mazzella <damianomazzella@gmail.com>
2022-11-22 19:00:38 +0100

    Added tomsfastmath.generate_prime

--- modules/ufastrsa/genprime.py
+++ modules/ufastrsa/genprime.py
@@ -5,6 +5,11 @@ try:
 
     pow3_ = tomsfastmath.exptmod
     invmod_ = tomsfastmath.invmod
+    generate_prime_ = tomsfastmath.generate_prime
+
+    def genprime(num=1024, test=25, safe=False):
+        return generate_prime_(num, test, safe)
+
 except ImportError:
     pow3_ = pow
 
@@ -15,6 +20,51 @@ except ImportError:
             a, c, d, b, e, f = b, e, f, a - q * b, c - q * e, d - q * f
         assert a >= 0 and c % g >= 0
         return a == 1 and c % g or 0
+
+    def miller_rabin_pass(a, n):
+        n_minus_one = n - 1
+        s, d = get_lowest_set_bit(n_minus_one)
+        a_to_power = pow3(a, d, n)
+        if a_to_power == 1:
+            return True
+        for i in range(s):
+            if a_to_power == n_minus_one:
+                return True
+            a_to_power = pow3(a_to_power, 2, n)
+        if a_to_power == n_minus_one:
+            return True
+        return False
+
+    class MillerRabinTest:
+        def __init__(self, randint, repeat):
+            self.randint = randint
+            self.repeat = repeat
+
+        def __call__(self, n):
+            randint = self.randint
+            n_minus_one = n - 1
+            for repeat in range(self.repeat):
+                a = randint(1, n_minus_one)
+                if not miller_rabin_pass(a, n):
+                    return False
+            return True
+
+    class GenPrime:
+        def __init__(self, getrandbits, testfn):
+            self.getrandbits = getrandbits
+            self.testfn = testfn
+
+        def __call__(self, bits):
+            getrandbits = self.getrandbits
+            testfn = self.testfn
+            while True:
+                p = (1 << (bits - 1)) | getrandbits(bits - 1) | 1
+                if p % 3 != 0 and p % 5 != 0 and p % 7 != 0 and testfn(p):
+                    break
+            return p
+
+    miller_rabin_test = MillerRabinTest(srandom.randint, 25)
+    genprime = GenPrime(srandom.getrandbits, miller_rabin_test)
 
 
 def pow3(x, y, z):
@@ -43,51 +93,6 @@ def gcd(a, b):
 
 def get_bit_length(n):
     return srandom.get_bit_length(n)
-
-
-def miller_rabin_pass(a, n):
-    n_minus_one = n - 1
-    s, d = get_lowest_set_bit(n_minus_one)
-    a_to_power = pow3(a, d, n)
-    if a_to_power == 1:
-        return True
-    for i in range(s):
-        if a_to_power == n_minus_one:
-            return True
-        a_to_power = pow3(a_to_power, 2, n)
-    if a_to_power == n_minus_one:
-        return True
-    return False
-
-
-class MillerRabinTest:
-    def __init__(self, randint, repeat):
-        self.randint = randint
-        self.repeat = repeat
-
-    def __call__(self, n):
-        randint = self.randint
-        n_minus_one = n - 1
-        for repeat in range(self.repeat):
-            a = randint(1, n_minus_one)
-            if not miller_rabin_pass(a, n):
-                return False
-        return True
-
-
-class GenPrime:
-    def __init__(self, getrandbits, testfn):
-        self.getrandbits = getrandbits
-        self.testfn = testfn
-
-    def __call__(self, bits):
-        getrandbits = self.getrandbits
-        testfn = self.testfn
-        while True:
-            p = (1 << (bits - 1)) | getrandbits(bits - 1) | 1
-            if p % 3 != 0 and p % 5 != 0 and p % 7 != 0 and testfn(p):
-                break
-        return p
 
 
 class GenRSA:
@@ -128,6 +133,4 @@ class GenRSA:
             return bits, n, e, d
 
 
-miller_rabin_test = MillerRabinTest(srandom.randint, 25)
-genprime = GenPrime(srandom.getrandbits, miller_rabin_test)
 genrsa = GenRSA(genprime)
